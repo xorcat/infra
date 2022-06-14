d373b8a9fee70ba18d77a7e0af2f449abced55e2
Aaron Blair <az@xor.cat>
2022-06-14 16:43:00 +1000

    adding remainder of DNS config

--- provision/terraform/gcp/main.tf
+++ provision/terraform/gcp/main.tf
@@ -43,3 +43,93 @@ resource "google_dns_record_set" "home" {
   ]
   ttl = 86400
 }
+
+resource "google_dns_record_set" "caa" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "CAA"
+  rrdatas = [
+    "0 issue \"letsencrypt.org\""
+  ]
+  ttl = 86400
+}
+
+resource "google_dns_record_set" "root-a" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "A"
+  rrdatas = [
+    data.sops_file.gcp_secrets.data["gcp_website_ip"]
+  ]
+  ttl = 86400
+}
+
+resource "google_dns_record_set" "www" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("www.%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "A"
+  rrdatas = [
+    data.sops_file.gcp_secrets.data["gcp_website_ip"]
+  ]
+  ttl = 86400
+}
+
+resource "google_dns_record_set" "mail" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("mail.%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "A"
+  rrdatas = [
+    data.sops_file.gcp_secrets.data["gcp_public_ip"]
+  ]
+  ttl = 86400
+}
+
+resource "google_dns_record_set" "mx" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "MX"
+  rrdatas = [
+    format("10 mail.%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  ]
+  ttl = 86400
+}
+
+resource "google_dns_record_set" "spf" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "TXT"
+  rrdatas = [
+    format("\"%s\"", data.sops_file.gcp_secrets.data["gcp_spf_record"])
+  ]
+  ttl = 86400
+}
+
+resource "google_dns_record_set" "dmarc" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("_dmarc.%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "TXT"
+  rrdatas = [
+    format("\"%s\"", data.sops_file.gcp_secrets.data["gcp_dmarc_record"])
+  ]
+  ttl = 86400
+}
+
+resource "google_dns_record_set" "dkim" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("dkimselector._domainkey.%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "TXT"
+  rrdatas = [
+    format("\"%s\"", data.sops_file.gcp_secrets.data["gcp_dkim_record"])
+  ]
+  ttl = 86400
+}
+
+resource "google_dns_record_set" "dns-wl" {
+  managed_zone = google_dns_managed_zone.domain.name
+  name         = format("_token._dnswl.%s.", data.sops_file.gcp_secrets.data["gcp_domain"])
+  type         = "TXT"
+  rrdatas = [
+    data.sops_file.gcp_secrets.data["gcp_dnswl_record"]
+  ]
+  ttl = 86400
+}
